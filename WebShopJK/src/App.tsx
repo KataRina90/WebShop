43ff6572b62ce2bb7a0a13154310d3be7e8dad37
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-07-14 15:43:59 +0200

    created simple navbar

--- WebShopJK/src/App.tsx
+++ WebShopJK/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import { Navbar } from './Components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <>
     <Navbar/>
-    </>
   )
 }
-
 export default App
--- WebShopJK/src/Components/Navbar.tsx
+++ WebShopJK/src/Components/Navbar.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-export const Navbar = () => {
+/* export function NavBar () {
   return (
-    <div>Navbar</div>
-  )
+    <Navbar bg="dark" color="blue" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/womens">Women's</Nav.Link>
+          <Nav.Link href="/mens">Men's</Nav.Link>
+          <Nav.Link href="/kids">Kids</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}; */
+
+export default function Navbar() {
+  return <nav className='nav'>
+    <a href='' className='site-title'> TShirts</a>
+    <ul>
+      <li>
+        <a href='/women'> Women </a>
+      </li>
+      <li>
+        <a href='/men'> Men </a> 
+        </li>
+    <li> 
+    <a href='/kids'> Kids  </a> 
+    </li>
+    <li> 
+    <a href='/kids'> About  </a> 
+    </li>
+    </ul>
+  </nav>
 }
+
