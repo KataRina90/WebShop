7c314b4685364e084d3817275969469242a666d3
Jena <jenagansivakumar@gmail.com>
2023-07-14 14:41:35 +0200

    added submit button

--- WebShopJK/src/Components/LogIn.tsx
+++ WebShopJK/src/Components/LogIn.tsx
@@ -1,23 +1,42 @@
-import React from "react";
+import { useState } from "react";
 
 export const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(password);
+    console.log(email);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email"> Email </label>
       <input
+        value={email}
         type="email"
         placeholder="your email"
         name="email"
         id="email"
+        onChange={handleEmailChange}
       ></input>
       <label htmlFor="password"> Password </label>
       <input
+        value={password}
         type="password"
         placeholder="minimum 8 characters"
-        name="email"
-        id="email"
-        min={8}
+        name="password"
+        id="password"
+        onChange={handlePasswordChange}
       ></input>
+      <button type="submit">Submit</button>
     </form>
   );
 };
