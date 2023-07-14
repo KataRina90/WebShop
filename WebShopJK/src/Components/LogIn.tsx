b8dbee4830ed77f69f3e170a298736e8af76cc4c
Jena <jenagansivakumar@gmail.com>
2023-07-14 12:52:23 +0200

    added login and password button

--- WebShopJK/src/Components/LogIn.tsx
+++ WebShopJK/src/Components/LogIn.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const LogIn = () => {
-  return <div>LogIng</div>;
+  return (
+    <form>
+      <label htmlFor="email"> Email </label>
+      <input
+        type="email"
+        placeholder="your email"
+        name="email"
+        id="email"
+      ></input>
+      <label htmlFor="password"> Password </label>
+      <input
+        type="password"
+        placeholder="minimum 8 characters"
+        name="email"
+        id="email"
+        min={8}
+      ></input>
+    </form>
+  );
 };
