a08846ecc63f1e709de80f5ce9c4e000ffbdd7d4
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-08-27 16:00:53 +0200

    initial layout

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,10 @@
 import { Color, IBrand, IProduct, Size } from "../Products/Product";
 import { ProductList } from "../Products/ProductList";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const brands: IBrand[] = [
     { brandName: 'Adidas', models: [] },
     { brandName: 'BOSS', models: [] },
@@ -12,7 +17,7 @@ const products: Array<IProduct> = [
         colors: [Color.white, Color.yellow, Color.pink],
         brand: brands[0],
         description: 'your perfect summer choice',
-        imageURL:'Images/white_adidas_01.jpg',
+        imageURL: 'Images/white_adidas_01.jpg',
         price: 50,
         sizes: [Size.XS, Size.S, Size.M]
     },
@@ -48,5 +53,18 @@ const products: Array<IProduct> = [
     }
 ]
 export default function App() {
-    return <ProductList list={products} />
+    return (
+        <Container>
+            <Row>
+                <Col sm={10} > <h1 style={{textAlign:'center'}}> TShirt </h1> </Col>
+                <Col sm={1}> <button> LogIn </button></Col>
+                <Col sm={1}> <button> Basket </button></Col>
+            </Row>
+            <Row>
+                <Col sm={4}> Search Filter </Col>
+                <Col sm={8}>  <ProductList list={products} /> </Col>
+            </Row>
+        </Container>
+
+    )
 }
