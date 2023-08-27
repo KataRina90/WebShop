8f189f9ecc1f18a25a3894a9a5d686c0bbf51e2a
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-08-27 16:20:55 +0200

    Add CardGroup to render product items

--- src/Products/ProductList.tsx
+++ src/Products/ProductList.tsx
@@ -1,14 +1,15 @@
 import { IProduct } from './Product';
 import { ProductItem } from './ProductItem';
+import {CardGroup} from 'react-bootstrap';
 export interface ProductListProps {
     list: Array <IProduct>
 }
 export function ProductList(props:ProductListProps) {
     return (
-        <div>
+        <CardGroup>
         {props.list.map (e=> <ProductItem item={e}/> )
         
         }
-        </div>
+        </CardGroup>
     )
 }
