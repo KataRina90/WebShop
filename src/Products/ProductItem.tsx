4c42dd72b37207fc99b21795536c5e71cb73867e
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-08-27 15:57:59 +0200

    initial layout and product item formatting

--- src/Products/ProductItem.tsx
+++ src/Products/ProductItem.tsx
@@ -1,15 +1,45 @@
 import { Color, IProduct, Size } from './Product';
-export interface ProductItemProps {
-    item:IProduct
-}
-export function ProductItem(props:ProductItemProps) {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
+
+function BasicExample() {
     return (
-        <div>
-            <img src={props.item.imageURL} width='300' height='300'/>
-            <p> {props.item.name} </p>
-            <p> {props.item.sizes.map(e => <span> {Size[e]}</span>)} </p>
-            <p> {props.item.colors.map(e => <span> {Color[e]}</span>)}</p>
-            <p> Price: {props.item.price} EUR </p>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default BasicExample;
+export interface ProductItemProps {
+    item: IProduct
+}
+export function ProductItem(props: ProductItemProps) {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant='top' src={props.item.imageURL} width='300' height='300' />
+            <CardHeader> {props.item.brand.brandName}</CardHeader>
+            <Card.Body>
+                <Card.Title> {props.item.name}</Card.Title>
+                <Card.Text>
+                    {props.item.sizes.map(e => <span> {Size[e]}</span>)}
+                </Card.Text>
+                <Card.Text>
+                    {props.item.colors.map(e => <span> {Color[e]}</span>)}
+                </Card.Text>
+                <Card.Text>
+                    Price: {props.item.price} EUR
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
