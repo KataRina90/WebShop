ea5c4146df48565bef45bf9313774ae81947def4
Jena <jenagansivakumar@gmail.com>
2023-08-28 21:29:51 +0200

    added filterProducts to SearchCriteria

--- src/search/SearchCriteria.tsx
+++ src/search/SearchCriteria.tsx
@@ -1,5 +1,5 @@
 import { Size, Color, SizeList, ColorList } from "../Products/Product";
-
+import { IProduct } from "../Products/Product";
 export interface ISearchCriteria {
   priceRange?: {
     from?: number;
@@ -7,4 +7,13 @@ export interface ISearchCriteria {
   };
   sizes: SizeList;
   colors: ColorList;
+}
+
+const filterFunction = (p: IProduct) => {
+  return true;
+};
+
+function filterProducts(criteria: ISearchCriteria, products: IProduct[]) {
+  const filteredProducts = products.filter(filterFunction);
+  return filterProducts;
 }
