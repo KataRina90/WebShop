71119fd1ae7e721985d0f822ac6d35710f764b28
Sinisa <sinisa_neskovic@yahoo.com>
2023-08-30 00:31:11 +0200

    fix dual wrapping of FilteredProductProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ProductListContext } from "./Products/ProductListContext";
-import { products } from "./Products/ProductListContext";
 import { BasketProvider } from "./basket/basketContext";
+import FilteredProductProvider from "./search/FilteredProductContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <BasketProvider>
-        <ProductListContext.Provider value={products}>
+        <FilteredProductProvider>
           <App />
-        </ProductListContext.Provider>
+        </FilteredProductProvider>
       </BasketProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -1,16 +1,15 @@
 import Colors from "./Colors";
 import PriceRange from "./PriceRange";
-import FilteredProductProvider from "./FilteredProductContext";
 import Sizes from "./Sizes";
 
 export default function SearchCriteria() {
   return (
-    <FilteredProductProvider>
+    <div>
       <div> Search Criteria</div>
       <PriceRange />
       <Colors />
       <Sizes />
-      <button>Search</button>
-    </FilteredProductProvider>
+    </div>
+
   );
 }
