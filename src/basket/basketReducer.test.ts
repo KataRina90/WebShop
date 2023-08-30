3c282067ee8e8160f1ade8ba0d181f3e849292cf
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-08-30 13:22:07 +0200

    remove test passed

--- src/basket/basketReducer.test.ts
+++ src/basket/basketReducer.test.ts
@@ -1,12 +1,13 @@
 //testiramo reducer da vidimo da li vraca dobro stanje 
 import { basketReducer } from "./basketReducer"
 import { IBasket } from "./basketStructure"
-const initBasket: IBasket = {
-    items: [],
-    totalItemsNo: 0,
-    totalPrice: 0
-}
-describe('Basket reducer testing', () => {
+
+describe('Basket reducer testing for ADD', () => {
+    const initBasket: IBasket = {
+        items: [],
+        totalItemsNo: 0,
+        totalPrice: 0
+    }
     test('adding new item to bakset when basket is empty', () => {
         let newBasket = basketReducer(initBasket, {
             type: "addItem",
@@ -58,3 +59,42 @@ describe('Basket reducer testing', () => {
         )
     })
 })
+
+
+describe('Basket reducer testing for REMOVE',()=> {
+    const initBasket: IBasket = {
+        items: [{
+            color: 'white',
+            price: 100,
+            productAmount: 2,
+            productId: '001',
+            size: 'S'
+        },
+        {
+            color: 'black',
+            price: 150,
+            productAmount: 3,
+            productId: '003',
+            size: 'L'
+        }],
+        totalItemsNo: 5,
+        totalPrice: 250
+    }
+    test ('removing item from a basket', ()=>{
+        let newBasket=basketReducer(initBasket,{
+          type:'removeItem',
+          productId:'003'
+        })
+    expect (newBasket).toEqual({
+        items: [{
+            color: 'white',
+            price: 100,
+            productAmount: 2,
+            productId: '001',
+            size: 'S'
+        }],
+        totalItemsNo: 2,
+        totalPrice: 100 
+    })
+    })
+}) 
