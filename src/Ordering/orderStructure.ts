b44c2826f5aa17fa521f34f6f35f28ed01d71536
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-09-16 17:45:46 +0200

    created order structure

---
+++ src/Ordering/orderStructure.ts
@@ -0,0 +1,12 @@
+import { IBasket } from "../basket/basketStructure";
+
+export interface IOrder {
+    orderItems: IBasket,
+    totalOrderItemsNo:number,
+    totalOrderPrice: number
+    orderNo: number
+}
+
+export interface IOrderList {
+    orders:IOrder
+}
