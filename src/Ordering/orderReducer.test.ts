dbdb1192bfc1cfd64db60c87d6e6ed50fcbd97a1
Katarina Neskovic <katarina.neskovic@gmail.com>
2023-09-20 16:09:19 +0200

    edit delete case in reducer

---
+++ src/Ordering/orderReducer.test.ts
@@ -0,0 +1,75 @@
+import { orderReducer } from "./orderReducer"
+import { IOrderList } from "./orderStructure"
+
+describe('Order reducer testing for new order', () => {
+    const initOrderList: IOrderList = {
+        orders: []
+    }
+    test('creating new order', () => {
+    let newOrderlist=orderReducer(initOrderList,{
+        type: "createOrder",
+        basket: {
+            items: [
+            {
+                productId: '001',
+                amountPrice: 100,
+                productAmount: 2,
+                color: 'white',
+                size: 'S'
+            },
+            {
+                productId: '002',
+                amountPrice: 150,
+                productAmount: 3,
+                color: 'yellow',
+                size: 'S'
+            },
+            {
+                productId: '003',
+                amountPrice: 200,
+                productAmount: 4,
+                color: 'black',
+                size: 'XL'
+            }
+            ],
+            totalItemsNo:3,
+            totalPrice:450
+        }
+    })
+    expect(newOrderlist).toEqual({
+    orders: [ {
+        orderDate:Date(),
+        orderItems: [
+            {
+                productId: '001',
+                amountPrice: 100,
+                productAmount: 2,
+                color: 'white',
+                size: 'S'
+            },
+            {
+                productId: '002',
+                amountPrice: 150,
+                productAmount: 3,
+                color: 'yellow',
+                size: 'S'
+            },
+            {
+                productId: '003',
+                amountPrice: 200,
+                productAmount: 4,
+                color: 'black',
+                size: 'XL'
+            }
+            ],
+            //proveri da li moze neki property da bude izostavljen sa toEqual
+            paid:true,
+            totalOrderItemsNo:3,
+            totalOrderPrice:450
+    
+    }
+    
+    ]
+    } as IOrderList)
+})
+})
--- src/Ordering/orderReducer.ts
+++ src/Ordering/orderReducer.ts
@@ -4,7 +4,7 @@ import { IOrderList } from "./orderStructure";
 export type OrderAction =
     | {
         type: "createOrder",
-        basket:IBasket ;
+        basket: IBasket;
 
     }
 
@@ -20,33 +20,54 @@ export type OrderAction =
 export function orderReducer(
     oldOrderList: IOrderList,
     action: OrderAction
-):IOrderList {
+): IOrderList {
     function generateRandomNo(): number {
         let minm = 100000
         let maxm = 999999
-        let randomNo=Math.floor(Math
-                .random() * (maxm - minm + 1)) + minm
-      let sameOrder=oldOrderList.orders.find((e)=>{
-        if (e.orderNo===randomNo)
-        return true
-        else false
-      }) 
-      if (sameOrder)
-      return generateRandomNo()
-      else return randomNo
-        
-    } 
-   
+        let randomNo = Math.floor(Math
+            .random() * (maxm - minm + 1)) + minm
+        let sameOrder = oldOrderList.orders.find((e) => {
+            if (e.orderNo === randomNo)
+                return true
+            else false
+        })
+        if (sameOrder)
+            return generateRandomNo()
+        else return randomNo
+
+    }
+
     switch (action.type) {
         case "deleteUnpaidOrder": {
+            //check type
+            const unpaidArray: IOrderList = {
+                orders: oldOrderList.orders.filter((p) => {
+                    p.paid === false
+                })
+            }
+
             return {
                 orders: oldOrderList.orders.filter((e) => {
-                    if (e.orderNo != action.orderNo && e.paid===true) {
+                    if (e.orderNo != action.orderNo &&
+                        unpaidArray.orders.find((j) => {
+                            if (j.orderNo === action.orderNo) return true
+                            else return false
+                        })) {
                         return true
                     }
                     else return false
+
                 })
             }
+
+           /*  return {
+                orders: oldOrderList.orders.filter((e) => {
+                    if (e.orderNo != action.orderNo) return true
+                    else if (e.paid) return true
+                    else return false
+                }
+                )
+            } */
 
         }
 
@@ -54,25 +75,25 @@ export function orderReducer(
         case "createOrder": {
 
             return {
-            orders:[
-                ...oldOrderList.orders,
-                {
-                   orderNo: generateRandomNo(),
-                   orderDate: Date(),
-                   paid:false,
-                   totalOrderItemsNo: action.basket.totalItemsNo,
-                   totalOrderPrice:action.basket.totalPrice,
-                   orderItems: action.basket.items.map((e)=>{
-                    return {
-                        productId:e.productId,
-                        amountPrice:e.amountPrice,
-                        color:e.color,
-                        productAmount:e.productAmount,
-                        size:e.size
+                orders: [
+                    ...oldOrderList.orders,
+                    {
+                        orderNo: generateRandomNo(),
+                        orderDate: Date(),
+                        paid: false,
+                        totalOrderItemsNo: action.basket.totalItemsNo,
+                        totalOrderPrice: action.basket.totalPrice,
+                        orderItems: action.basket.items.map((e) => {
+                            return {
+                                productId: e.productId,
+                                amountPrice: e.amountPrice,
+                                color: e.color,
+                                productAmount: e.productAmount,
+                                size: e.size
+                            }
+                        })
                     }
-                   })
-                }
-            ]
+                ]
             }
         }
         default: return oldOrderList
